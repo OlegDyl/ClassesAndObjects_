c1ae6815daa41d09ef6fbfcbe3dc6ae0330c6c4f
OlegDyl <ommichail@yandex.ru>
2023-06-09 20:42:44 +0300

    Add lesson

--- ClassesAndObjects/src/main/kotlin/module8/Car.kt
+++ ClassesAndObjects/src/main/kotlin/module8/Car.kt
@@ -1,5 +1,32 @@
 package module8
 
-class Car(val brand: String, val model: String, val color: String) {
+import kotlin.random.Random
 
+class Car(val brand: String, val model: String, val color: String = "White") {
+
+    constructor(descriptor: Pair<String, String>, color: String): this(
+        brand = descriptor.first,
+        model = descriptor.second,
+        color = color)
+
+    init {
+        println("Car init")
+    }
+
+    val currentSpeed = 0.0
+
+    init {
+
+        println("Second init")
+    }
+    fun move() {
+        val distance = Random.nextInt(1, 10)
+        println("Car passed $distance km")
+    }
+    fun start (){
+        println(" Car started")
+    }
+    fun stop() {
+        println(" Car stopped")
+    }
 }
--- ClassesAndObjects/src/main/kotlin/module8/main.kt
+++ ClassesAndObjects/src/main/kotlin/module8/main.kt
@@ -1,14 +1,21 @@
 package module8
 
 fun main() {
+  /*  val vw = Car(brand = "VW", model = "Beetle", color = "Green")
+    val lada = Car(brand = "Lada", model = "Vesta", color = "Yellow")
+    val bmw = Car(brand = "BMW", model = "X6")
+
+    printInfo(vw)
+    printInfo(lada)
+    printInfo(bmw)*/
     val vw = Car(brand = "VW", model = "Beetle", color = "Green")
-    val lada = Car(brand = "Lada", model = "Vesta", color = "Black")
-    val bmw = Car(brand = "BMW", model = "3-series", color = "Yellow")
+    val renault = Car(descriptor = "Renault" to "Logan", color = "Brown")
 
-   // printInfo(vw)
-
-    fun printInfo(car: Car){
-        println("Brand - ${car.brand}, model - ${car.model}, color - ${car.color}")
-    }
-
+    printInfo(vw)
+    printInfo(renault)
 }
+
+fun printInfo(car: Car) {
+    println("Brand - ${car.brand}, model - ${car.model}, color - ${car.color}")
+}
+
